e59b78570a1b1736375d324b0c23722a6641a2d3
LAPTOP-NKUME37F\ASUS <phungvandung030623@gmail.com>
2024-09-30 19:41:00 +0700

    homework update

--- flutter_basic/lib/State_Management/login/login_screen.dart
+++ flutter_basic/lib/State_Management/login/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_basic/State_Management/login/bloc/logic_bloc.dart';
 import 'package:flutter_basic/State_Management/login/widgets/app_icon_section.dart';
 import 'package:flutter_basic/State_Management/login/widgets/forgot_password_section.dart';
+import 'package:flutter_basic/State_Management/login/widgets/loading_overlay.dart';
 import 'package:flutter_basic/State_Management/login/widgets/login_button_section.dart';
 import 'package:flutter_basic/State_Management/login/widgets/other_sign_in_method_section.dart';
 import 'package:flutter_basic/State_Management/login/widgets/username_password_section.dart';
@@ -17,11 +18,147 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   late final TextEditingController userNameTextController;
   late final TextEditingController passwordTextController;
+
   @override
-  void initState() {
-    super.initState();
-    userNameTextController = TextEditingController();
-    passwordTextController = TextEditingController();
+  Widget build(BuildContext context) {
+    //! BlocProvider
+    //? được sử dụng để tạo một instance của LoginBloc và cung cấp nó cho cây widget con.
+    //? Điều này đảm bảo rằng LoginBloc có thể được truy cập từ bất kỳ widget nào bên trong cây mà cần tới nó.
+    return BlocProvider(
+      create: (context) => LoginBloc(),
+      child: Scaffold(
+        body: Builder(builder: (context) {
+          //! BlocListener
+
+          return BlocConsumer<LoginBloc, LoginState>(
+            //? được dùng để lắng nghe các thay đổi trạng thái từ LoginBloc và phản hồi (thông qua UI)
+            //? dựa trên các trạng thái này.
+            listener: (context, state) {
+              //?  Trong trường hợp này, nó đang lắng nghe các trạng thái thất bại và thành công của quá trình đăng nhập.
+              if (state is FailedLoginState) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                    state.errorMessage ?? '',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  backgroundColor: Colors.red,
+                ));
+              } else if (state is SuccessfullyLoginState) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                    state.successfulMsg ?? '',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  backgroundColor: Colors.green,
+                ));
+              }
+            },
+            builder: (_, state) {
+              final isLoading = state is LoadingLoginState;
+              return Stack(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      FocusScope.of(context).unfocus();
+                    },
+                    behavior: HitTestBehavior
+                        .opaque, // Ensure taps on the empty space are registered
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 36),
+                      child: SafeArea(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 45),
+                                child: AppIconSection(),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 43),
+                                child: Column(
+                                  children: [
+                                    UsernamePasswordSection(
+                                      userNameTextController:
+                                          userNameTextController,
+                                      passwordTextController:
+                                          passwordTextController,
+                                    ),
+                                    ForgotPasswordSection(
+                                      onPressed: () {
+                                        Navigator.of(context)
+                                            .pushNamed("forgot-password");
+                                      },
+                                    ),
+                                    // //! BlocBuilder
+                                    // //? được sử dụng để xây dựng UI phụ thuộc vào trạng thái hiện tại của LoginBloc
+                                    // BlocBuilder<LoginBloc, LoginState>(
+                                    //   builder: (context, state) {
+                                    //     //? Trong trường hợp này, BlocBuilder xác định
+                                    //     //? nếu trạng thái là LoadingLoginState để hiển thị giao diện loading.
+                                    //     final isLoading =
+                                    //         state is LoadingLoginState;
+                                    //     return LoginButtonSection(
+                                    //       isLoading: isLoading,
+                                    //       onPressed: () {
+                                    //         //? Khi nút đăng nhập được nhấn,
+                                    //         //? một sự kiện LoginWithUsernamePassword sẽ được gửi tới LoginBloc
+                                    //         //? với tên người dùng và mật khẩu từ các trường nhập liệu.
+                                    //         BlocProvider.of<LoginBloc>(context)
+                                    //             .add(LoginWithUsernamePassword(
+                                    //                 username:
+                                    //                     userNameTextController
+                                    //                         .text,
+                                    //                 pasword:
+                                    //                     passwordTextController
+                                    //                         .text));
+                                    //       },
+                                    //     );
+                                    //   },
+                                    // ),
+                                    LoginButtonSection(
+                                      onPressed: () {
+                                        //? Khi nút đăng nhập được nhấn,
+                                        //? một sự kiện LoginWithUsernamePassword sẽ được gửi tới LoginBloc
+                                        //? với tên người dùng và mật khẩu từ các trường nhập liệu.
+                                        BlocProvider.of<LoginBloc>(context)
+                                            .add(LoginWithUsernamePassword(
+                                          username: userNameTextController.text,
+                                          password: passwordTextController.text,
+                                        ));
+                                      },
+                                    )
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 45,
+                              ),
+                              OtherSignInMethodSection(
+                                onGoogle: () {
+                                  BlocProvider.of<LoginBloc>(context)
+                                      .add(LoginWithThirdParty(isGoogle: true));
+                                },
+                                onFacebook: () {
+                                  BlocProvider.of<LoginBloc>(context).add(
+                                      LoginWithThirdParty(isGoogle: false));
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  if (isLoading) const LoadingOverylay()
+                ],
+              );
+            },
+          );
+        }),
+      ),
+    );
   }
 
   @override
@@ -31,100 +168,10 @@ class _LoginScreenState extends State<LoginScreen> {
     super.dispose();
   }
 
-  void _onLogin(BuildContext context) {
-    BlocProvider.of<LoginBloc>(context).add(LoginWithUsernamePassword(
-      username: userNameTextController.text,
-      password: passwordTextController.text,
-    ));
-  }
-
-  void _onForgotPassword(BuildContext context) {
-    BlocProvider.of<LoginBloc>(context).add(
-      ForgotPassword(username: userNameTextController.text),
-    );
-  }
-
-  void _showSnackbar(BuildContext context, String message, Color color) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-          content: Text(message, style: const TextStyle(color: Colors.white)),
-          backgroundColor: color),
-    );
-  }
-
   @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LoginBloc(),
-      child: Scaffold(
-        body: BlocListener<LoginBloc, LoginState>(
-          listener: (context, state) {
-            if (state is ForgotPasswordSuccess) {
-              _showSnackbar(
-                  context,
-                  state.successfulMsg ?? 'Password reset successful',
-                  Colors.green);
-              Navigator.pushNamed(context, "forgot_password");
-            } else if (state is FailedLoginState) {
-              _showSnackbar(
-                  context, state.errorMessage ?? 'Login failed', Colors.red);
-            } else if (state is SuccessfullyLoginState) {
-              _showSnackbar(context, state.successfulMsg ?? 'Login successful',
-                  Colors.green);
-              Navigator.pushNamed(context, "home");
-            }
-          },
-          child: GestureDetector(
-            onTap: () => FocusScope.of(context).unfocus(),
-            child: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 36),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 45),
-                      const AppIconSection(),
-                      const SizedBox(height: 43),
-                      UsernamePasswordSection(
-                        userNameTextController: userNameTextController,
-                        passwordTextController: passwordTextController,
-                      ),
-                      BlocBuilder<LoginBloc, LoginState>(
-                        builder: (context, state) {
-                          return ForgotPasswordSection(
-                              onPressed: () => _onForgotPassword(context));
-                        },
-                      ),
-                      const SizedBox(height: 45),
-                      BlocBuilder<LoginBloc, LoginState>(
-                        builder: (context, state) {
-                          final isLoading = state is LoadingLoginState;
-                          return LoginButtonSection(
-                              isLoading: isLoading,
-                              onPressed: () => _onLogin(context));
-                        },
-                      ),
-                      const SizedBox(height: 45),
-                      BlocBuilder<LoginBloc, LoginState>(
-                        builder: (context, state) {
-                          return OtherSignInMethodSection(
-                            onGoogle: () => BlocProvider.of<LoginBloc>(context)
-                                .add(LoginWithThirdParty(isGoogle: true)),
-                            onFacebook: () =>
-                                BlocProvider.of<LoginBloc>(context)
-                                    .add(LoginWithThirdParty(isGoogle: false)),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+  void initState() {
+    userNameTextController = TextEditingController();
+    passwordTextController = TextEditingController();
+    super.initState();
   }
 }
